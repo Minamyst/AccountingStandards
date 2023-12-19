8e697e909d54e56f06fda8ab3f9843f79e851557
Minamyst <vakula.echambadi@gmail.com>
2023-12-19 10:19:44 +0530

    Create Accounting Standards.py

---
+++ Accounting Standards.py
@@ -0,0 +1,31 @@
+from datetime import datetime, date, timedelta
+
+today = date.today()
+current_time = datetime.now().time()
+now = datetime.now()
+
+
+class FinancialYear:
+    def __init__(self, start_year):
+        self.start_year = start_year
+
+    def get_current_financial_year(self):
+        start_date = datetime(self.start_year, 4, 1)
+        end_date = datetime(self.start_year + 1, 3, 31)
+
+        return start_date, end_date
+
+
+start_year_input = input("Enter the current year of financial year: ")
+start_year = int(start_year_input)
+
+financial_year = FinancialYear(start_year)
+
+start_date, end_date = financial_year.get_current_financial_year()
+
+print("Financial Year Start Date:", start_date.strftime("%d-%m-%y"))
+print("Financial Year End Date:", end_date.strftime("%d-%m-%y"))
+
+
+
+
